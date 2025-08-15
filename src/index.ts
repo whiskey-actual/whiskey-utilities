03eccf2df0000bad5345ac6f9fd09fd865577f7b
whiskey-actual <actualwhiskey@gmail.com>
2025-08-15 14:43:07 -0400

    tweak

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,21 @@ import dates from './dates';
 import progress from './progress';
 import strings from './strings';
 import objects from './objects';
-import functions from './functions';
 import filesystem from './filesystem';
+import functions from './functions';
+import lists from './lists';
 import numbers from './numbers';
 import sanitizeInput from './sanitizeInput';
+
 
 export default {
   dates,
   filesystem,
   functions,
+  lists,
   numbers,
   objects,
   progress,
   sanitizeInput,
-  strings,  
+  strings,
 }
