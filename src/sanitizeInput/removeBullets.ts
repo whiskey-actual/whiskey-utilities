c3c670d06c1bc4010d8fc21b5a2161a5577db3fd
whiskey-actual <actualwhiskey@gmail.com>
2025-08-15 15:02:49 -0400

    tweak

--- src/sanitizeInput/removeBullets.ts
+++ src/sanitizeInput/removeBullets.ts
@@ -1,15 +1,22 @@
-export default function removeBullet(s:string, prefixWordsToRemove:string[]=[]) {
-    let output:string=s
-    output = output.replace(/^\s?\d*[\.\:]?\s+/,'').trim()
-    output = output.replace(/^\s?-\s+/, '').trim()
+export default function removeBulletsFromList(bulletedList:string[], prefixWordsToRemove:string[]=[]):string[] {
+    
+    let output:string[] = bulletedList
 
+    // first generate the regex for any prefixes
     let regexWords:string[]=[]
     prefixWordsToRemove.map((prefix)=>{
         regexWords.push(`[${prefix.charAt(0).toUpperCase()}${prefix.charAt(0).toLowerCase()}]${prefix.slice(1)}`)
     })
-
     const regexWordReplacement:RegExp = new RegExp(`^(${regexWords.join("|")})?\s?\d*\s?\:?\s?`)
-    output = output.replace(regexWordReplacement, '')
     
-    return output.trim()
+    for(let i=0; i<output.length; i++) {
+        output[i] = output[i].replace(/^\s?\d*[\.\:-]?\s+/,'').trim()
+        output = output
+        if(prefixWordsToRemove.length>0) {
+            output[i] = output[i].replace(regexWordReplacement, '').trim()
+        }
+    }
+    
+    return output
+    
 }
